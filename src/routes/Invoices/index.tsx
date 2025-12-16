490c16842cd2687ba810e105017a7bddebaeee2c
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-16 10:25:36 -0300

    Active links

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,5 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../../data";
+import "./styles.css";
 
 export default function Invoices() {
 
@@ -14,13 +15,13 @@ export default function Invoices() {
                 }}
             >
                 {invoices.map((invoice) => (
-                    <Link
-                        style={{ display: "block", margin: "1rem 0" }}
+                    <NavLink
+                        className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
                         to={`/invoices/${invoice.number}`}
                         key={invoice.number}
                     >
                         {invoice.name}
-                    </Link>
+                    </NavLink>
                 ))}
             </nav>
             <Outlet />
